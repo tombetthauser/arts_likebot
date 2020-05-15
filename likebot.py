f34c78250b9f82cb721f1ebde4d51996d720a184
tombetthauser <tombetthauser@gmail.com>
2020-05-14 17:32:24 -0700

    Get login working and part of fetch_followers

--- likebot.py
+++ likebot.py
@@ -1,10 +1,59 @@
 from selenium import webdriver
 from time import sleep
+from getpass import getpass
+
 
 class LikeBot:
-  def __init__(self):
+  def __init__(self, username, password):
+    self.username = username or input("Please input your username: ")
+    self.password = password or getpass("Please input your password (private / not saved): ")
     self.driver = webdriver.Chrome()
     self.driver.get("https://instagram.com")
     sleep(2)
 
-LikeBot()
+  def login(self):
+    # fill in username
+    login_name = self.driver.find_element_by_xpath(
+        "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input"
+    ).send_keys(self.username)
+
+    # fill in password
+    login_pass = self.driver.find_element_by_xpath(
+        "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input"
+    ).send_keys(self.password)
+    sleep(1)
+
+    # delete password from temporary variable
+    self.password = None
+
+    # click log in button
+    self.driver.find_element_by_xpath(
+        "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]"
+    ).click()
+    sleep(3)
+
+    # click not now button to close pop up modal
+    self.driver.find_element_by_xpath(
+        "/html/body/div[4]/div/div/div[3]/button[2]"
+    ).click()
+    sleep(1)
+
+  def fetch_followers(self):
+    # click users profile link
+    self.driver.find_element_by_xpath(
+        "/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[5]/a/img"
+    ).click()
+    sleep(1)
+
+    # click followers link
+    self.driver.find_element_by_xpath(
+        "/html/body/div[1]/section/main/div/header/section/ul/li[2]/a"
+    ).click()
+    sleep(1)
+
+    # scroll to bottom of followers modal
+    self.driver.execute_script("window.scrollTo(0, Y)")
+
+testbot = LikeBot("tombetthauser", "Ch1pD00d3?")
+testbot.login()
+testbot.fetch_followers()
